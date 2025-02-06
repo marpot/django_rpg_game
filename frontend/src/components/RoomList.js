db812458a3ec3ef61b65753e056eb08b55cf8afc
Marcin Potoczny <marcin.potoczny@protonmail.com>
2025-02-06 16:55:50 +0100

    change buttons

--- frontend/src/components/RoomList.js
+++ frontend/src/components/RoomList.js
@@ -7,11 +7,11 @@ const RoomList = ({ rooms, onRoomClick }) => {
         {rooms.map((room) => (
           <div className="column is-one-third" key={room.id}>
             <div
-              className="box p-4 is-flex is-flex-direction-column has-text-centered room-box"
+              className="box p-4 is-flex is-flex-direction-column has-text-centered room-box has-background-primary-15"
               onClick={() => onRoomClick(room.id)}
             >
               <h3 className="title is-4 has-text-primary">{room.name}</h3>
-              <p className="subtitle is-6 has-text-grey">Dołącz do tego pokoju i rozpocznij grę!</p>
+              <p className="subtitle is-6 has-text-white">Dołącz do tego pokoju i rozpocznij grę!</p>
             </div>
           </div>
         ))}
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
   return (
     <aside className="menu">
       <p className="menu-label mt-5 has-text-centered">Panel kontrolny</p>
       <ul className="menu-list has-text-centered">
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/profile">Profil</Link></li>
-        <li><Link to="/settings">Ustawienia</Link></li>
-        <li><Link to="/">Wyloguj się</Link></li>
+        <button className="button is-fullwidth is-primary mt-5" onClick={() => navigate('/dashboard')}>Dashboard</button>
+        <button className="button is-fullwidth is-primary mt-5" onClick={() => navigate('/profile')}>Profil</button>
+        <button className="button is-fullwidth is-primary mt-5" onClick={() => navigate('/settings')}>Ustawienia</button>
+        <button className="button is-fullwidth is-primary mt-5" onClick={() => navigate('/')}>Wyloguj się</button>
       </ul>
     </aside>
   );
