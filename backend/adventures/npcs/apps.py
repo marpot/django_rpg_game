b79a04c288e58884959a45bfac548e4cc013736c
marpot <marcin.potoczny@protonmail.com>
2026-04-15 11:41:32 +0200

    refractor: remove unused sub-app configs from adventures modules

--- backend/adventures/npcs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class NPCsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adventures.npcs'
-    label = 'adventures_npcs' 
